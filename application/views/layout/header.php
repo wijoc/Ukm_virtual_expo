e76489332a65484d286f0f8163e1b9e2affd4b9b
Joe Wicaksono <wicaksonojoshua@gmail.com>
2021-01-21 16:47:47 +0700

    new branch katalis
    branch untuk kerjaan katalis

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -25,7 +25,7 @@
         <a href="index3.html" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Toko</a>
+        <a href="<?php echo site_url('Toko_c') ?>" class="nav-link">Toko</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="<?php echo site_url('Auth/logoutProses') ?>" class="nav-link">Logout</a>
--- application/views/visitor/pameran_content.php
+++ application/views/visitor/pameran_content.php
@@ -1,21 +1,62 @@
 <div class="banner">
     <div class="container">
       <div class="row" data-aos="zoom-out">
-        <?php foreach($dataKategori as $showKtgr): ?>
-          <a href="<?php echo site_url('Visitor_c/expoCategoryStore/').urlencode(base64_encode($showKtgr['ktgr_id'])) ?>">
-            <div class="col-md-6 mb-3">
+        <!-- Kategori Batik -->
+          <a href="<?php echo site_url('Visitor_c/expoCategoryStore/4') ?>">
+            <div class="col-lg-12 col-md-6 mb-3">
               <div class="card border border-dark border-rounded">
-                <img class="card-img-top card-img" src="<?php echo base_url().$showKtgr['ktgr_banner'] ?>" alt="">
+                <img class="card-img-top card-img" src="<?php echo base_url().'assets/expo_img/kategori_img/batik-floral-pattern1123.png' ?>" alt="">
                 <div class="text-img col-12">
-                  <h2><?php echo $showKtgr['ktgr_nama'] ?></h2>
+                  <h2>BATIK, FASHION & KECANTIKAN</h2>
                 </div>
                 <div class="card-footer">
-                  <a class="col-12 btn btn-opacity-info" href="<?php echo site_url('Visitor_c/expoCategoryStore/').urlencode(base64_encode($showKtgr['ktgr_id'])) ?>" style="color: black; font-weight: bold"> Lihat Galeri Pameran </a>
+                  <a class="col-12 btn btn-opacity-info" href="<?php echo site_url('Visitor_c/expoCategoryStore/4') ?>" style="color: black; font-weight: bold"> Lihat Galeri Pameran </a>
                 </div>
               </div>
             </div>
           </a>
-        <?php endforeach ?>
+        <!-- Kategori Kopi Lelet -->
+          <a href="<?php echo site_url('Visitor_c/expoCategoryStore/3') ?>">
+            <div class="col-md-4 mb-3">
+              <div class="card border border-dark border-rounded">
+                <img class="card-img-top card-img" src="<?php echo base_url().'assets/expo_img/kategori_img/4848667.jpg' ?>" alt="">
+                <div class="text-img col-12">
+                  <h2>KOPI LELET</h2>
+                </div>
+                <div class="card-footer">
+                  <a class="col-12 btn btn-opacity-info" href="<?php echo site_url('Visitor_c/expoCategoryStore/3') ?>" style="color: black; font-weight: bold"> Lihat Galeri Pameran </a>
+                </div>
+              </div>
+            </div>
+          </a>
+        <!-- Kategori Wisata -->
+          <a href="<?php echo site_url('Visitor_c/expoCategoryStore/2') ?>">
+            <div class="col-md-4 mb-3">
+              <div class="card border border-dark border-rounded">
+                <img class="card-img-top card-img" src="<?php echo base_url().'assets/expo_img/kategori_img/category_banner.png' ?>" alt="">
+                <div class="text-img col-12">
+                  <h2>WISATA REMBANG</h2>
+                </div>
+                <div class="card-footer">
+                  <a class="col-12 btn btn-opacity-info" href="<?php echo site_url('Visitor_c/expoCategoryStore/2') ?>" style="color: black; font-weight: bold"> Lihat Galeri Pameran </a>
+                </div>
+              </div>
+            </div>
+          </a>
+        <!-- Kategori Batik -->
+          <a href="<?php echo site_url('Visitor_c/expoCategoryStore/1') ?>">
+            <div class="col-md-4 mb-3">
+              <div class="card border border-dark border-rounded">
+                <img class="card-img-top card-img" src="<?php echo base_url().'assets/expo_img/kategori_img/4252349.jpg' ?>" alt="">
+                <div class="text-img col-12">
+                  <h2>KULINER REMBANG</h2>
+                </div>
+                <div class="card-footer">
+                  <a class="col-12 btn btn-opacity-info" href="<?php echo site_url('Visitor_c/expoCategoryStore/1') ?>" style="color: black; font-weight: bold"> Lihat Galeri Pameran </a>
+                </div>
+              </div>
+            </div>
+          </a>
       </div>
     </div>
 </div>
